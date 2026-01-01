1ae5ea2913c03fc019b4bc3e3ee21ad51d3a2a0a
Shamitsu212 <darklin314@gmail.com>
2026-01-01 20:19:31 +0300

    feat: Сверстана секция с постами
    Co-authored-by: VicelikePath <vicelikepath@gmail.com>

---
+++ my-vue-app/src/components/Sections/Welcome_Section/Welcome_Section.vue
@@ -0,0 +1,64 @@
+<script setup>
+    import MediumLogo from '../../UI/Logo/MediumLogo.vue';
+</script>
+
+<template>
+    <div class="Section">
+
+        <div class="Section__Content">
+            <div class="Content__Logo">
+                <MediumLogo />
+            </div>
+
+            <h1 class="Content__H">
+                <span class="H__Text">Welcome to Calm – a lifestyle blog about</span>
+                <span class="H__Text">minimalism, technology, design & travel.</span>
+            </h1>
+        </div>
+        
+       
+    </div>
+</template>
+
+<style scoped>
+    .Section{
+        background-color: white;
+
+        width: 1440px;
+        height: 438px;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    .Section__Content{
+        width: 800px;
+        height: 148px;
+        
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 17px;
+    }
+
+    .Content__Logo{
+        width: 94px;
+        height: 48px;
+    }
+    .Content__H{
+        width: 683px;
+        height: 80px;
+
+        display: flex;
+        flex-direction: column;
+    }
+    .H__Text{
+        font-weight: 400;
+        font-style: Regular;
+        font-size: 36px;
+        line-height: 40px;
+        letter-spacing: 0;
+        color: #353732;
+    }
+</style>
---
+++ my-vue-app/src/components/UI/PostImages/MediumPostImage.vue
@@ -0,0 +1,111 @@
+<script setup>
+
+    const props = defineProps({
+        
+        theme:{
+            type: String,
+            default: 'Travel' 
+        },
+        title:{
+            type: String,
+            default: 'Title..' 
+        },
+        date: {
+            type: String,
+            default: 'Month date year' 
+        },
+        src: {
+            type: String,
+            required: true
+        }
+    })
+</script>
+
+<template>
+    <div class="Post">
+
+        <div class="Post__wrapper">
+            <img :src="src" class="wrapper__img"/>
+        </div>
+
+        <div class="Post__Content">
+            <div class="Content__Theme">
+                {{ theme }}
+            </div>
+            <h2 class="Content__Title">
+                {{ title }}
+            </h2>
+            <time class="Content__Date">
+                {{ date }}
+            </time>
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+    .Post{
+        width: 374px;
+        height: 332px;
+
+        display: flex;
+        flex-direction: column;
+
+        background-color: white;
+    }
+    .Post__wrapper{
+        width: 374px;
+        height: 210px;
+    }
+    .wrapper__img{
+        width: 100%;
+        height: 100%;
+    }
+    .Post__Content{
+        width: 374px;
+        height: 121px;
+
+        display: flex;
+        flex-direction: column;
+        padding-left: 20px;
+    }
+    .Content__Theme{
+        width: 42px;
+        height: 12px;
+
+        color: #353732;
+        font-size: 12px;
+        line-height: 12px;
+        letter-spacing: 2px;
+        font-weight: 400;
+        font-style: Regular;
+
+        margin-top: 20px;
+    }
+    .Content__Title{
+        min-width: 304px;
+        height: 24px;
+
+        color: #45525B;
+        font-size: 17px;
+        line-height: 24px;
+        letter-spacing: 0;
+        font-weight: 400;
+        font-style: Regular;
+
+        margin-top: 10px;
+    }
+    .Content__Date{
+        min-width: 98px;
+        height: 17px;
+
+        color: #45525B;
+        font-size: 11px;
+        line-height: 26.8px;
+        letter-spacing: 0;
+        font-weight: 400;
+        font-style: Regular;
+
+        margin-top: 10px;
+    }
+</style>
